a347dfa7a8d0d37048ec00ed76c5adf0dcf0a5b7
Rohit <prjagtap90@gmail.com>
2022-09-16 12:40:22 +0530

    created component

---
+++ weatherapp/src/components/weather/Weather.js
@@ -0,0 +1,13 @@
+// d33e7b433c0321b62f577b219b84174b
+// https://api.openweathermap.org/data/2.5/weather?q=kalyan&appid=46f78d49915a3be2c00e75baaec5b8be 
+
+import "./weather.css"
+import React from 'react'
+
+const Weather = () => {
+  return (
+    <div>Weather</div>
+  )
+}
+
+export default Weather
